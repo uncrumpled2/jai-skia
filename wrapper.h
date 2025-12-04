b1a2c7c834d9802e2ba0e93033d8d6bcfaf39424
Timothy C Eichler <timeyyy_da_man@hotmail.com>
2025-12-04 10:08:47 +1000

    add skfont

--- wrapper.h
+++ wrapper.h
@@ -3,6 +3,9 @@
 
 #include "include/core/SkCanvas.h"
 #include "include/core/SkColor.h"
+#include "include/core/SkFont.h"
+#include "include/core/SkFontMetrics.h"
+#include "include/core/SkFontMgr.h"
 #include "include/core/SkImage.h"
 #include "include/core/SkPaint.h"
 #include "include/core/SkPath.h"
@@ -10,6 +13,7 @@
 #include "include/core/SkSurface.h"
 #include "include/core/SkTypeface.h"
 #include "include/core/SkStream.h"
+#include "include/core/SkTextBlob.h"
 #include "include/encode/SkPngEncoder.h"
 
 #endif // SKIA_WRAPPER_H
